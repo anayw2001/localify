375554e260289b00a3d7e107795be417bddc58a2
Anay Wadhera <awadhera@berkeley.edu>
2021-09-18 19:57:39 -0700

    api: complete playlist api

--- api.py
+++ api.py
@@ -1,9 +1,22 @@
-from json.decoder import JSONDecoder
+import json
+from json.decoder import JSONDecodeError, JSONDecoder
+from json.encoder import JSONEncoder
 from flask_restful import Resource
 from classes.playlist import Playlist, deserialize_playlist
 from classes.location import Location
 from classes.song import Song
 from classes.user import get_user, register_user
+from classes.user import User
+
+def update(user: User):
+    f = open("users.json", "r+")
+    data = {}
+    try:
+        data = json.load(f)
+    except JSONDecodeError:
+        pass
+    data.update({user.uid:user.get_user_json()})
+    open("users.json", "w+").write(JSONEncoder().encode(data))
 
 class UserAPI(Resource):
     def get(self, uid):
@@ -15,7 +28,12 @@ class UserAPI(Resource):
 class PlaylistAPI(Resource):
     def get(self, json):
         user_dict = JSONDecoder().decode(json)
-        usr = get_user(user_dict['user'])
-        for playlist in user_dict['playlists']:
-            deserialize_playlist(playlist)
-        
+        usr = get_user(user_dict['user_uid'])
+        if user_dict['playlist_uid'] in usr.playlists:
+            return usr.playlists['playlist_uid']
+
+    def put(self, json):
+        req_dict = JSONDecoder().decode(json)
+        usr = get_user(req_dict['user_uid'])
+        usr.add_playlist(Playlist(req_dict['name'], Location(req_dict['lat'], req_dict['long'], req_dict['pl_name'])))
+        update(usr)
--- classes/user.py
+++ classes/user.py
@@ -5,29 +5,28 @@ import json
 from os import write
 import uuid
 
-from classes.playlist import deserialize_playlist
+from classes.playlist import Playlist, deserialize_playlist
 
 class User:
-    def __init__(self, username, uid=None, playlists=[]) -> None:
+    def __init__(self, username:str, uid:str=None, playlists:dict={}) -> None:
         self.playlists = playlists
         if not uid:
             uid = str(uuid.uuid4())
         self.uid = uid
         self.username = username
 
-    def add_playlist(self, p):
-        self.playlists += [p]
-        self.playlists = sorted(self.playlists)
+    def add_playlist(self, p: Playlist) -> None:
+        self.playlists.update({p.uid:p})
 
-    def remove_playlist(self, p):
-        self.playlists.remove(p)
+    def remove_playlist(self, p: Playlist) -> None:
+        self.playlists.pop(p.uid)
 
     # api methods
     def get_user_json(self) -> str:
         export_dict = {}
         export_dict.update({"name":self.username})
         export_dict.update({"uid":str(self.uid)})
-        export_dict.update({"playlists":[playlist.get_as_dict() for playlist in self.playlists]})
+        export_dict.update({"playlists":[self.playlists[uid].get_as_dict() for uid in self.playlists]})
         return JSONEncoder().encode(export_dict)
     
 def deserialize_user(json_str:str) -> User:
@@ -38,11 +37,11 @@ def deserialize_user(json_str:str) -> User:
     return User(name, uid, playlists)
 
 
-def get_user(uid: str) -> bytes:
+def get_user(uid: str) -> User:
     f = open("users.json", "r")
     data = json.load(f)
     if uid in data:
-        return base64.b64encode(deserialize_user(data[uid]))
+        return deserialize_user(data[uid])
     else:
         return None
 
@@ -62,7 +61,7 @@ def register_user(json_str: str) -> int:
     newUser = User(new_data['name'])
     data.update({newUser.uid:newUser.get_user_json()})
     print(data)
-    open("users.json", "w+").write(json.dumps(JSONEncoder().encode(data), indent=4))
+    open("users.json", "w+").write(JSONEncoder().encode(data))
     return 0
     
 
